1b9a813e62044610009b01a0da64c53c5e7176f1
BenDavies1218 <148213274+BenDavies1218@users.noreply.github.com>
2024-08-14 19:27:27 +1000

    feat: Routing and File Creation
    I create the basic routing and all the pages i should need.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Template from "./pages/_Template";
+import PrivacyPage from "./pages/PrivacyPage";
+import TosPage from "./pages/TosPage";
+import AllProductsPage from "./pages/AllProductsPage";
+import SingleProductPage from "./pages/SingleProductPage";
+import Cart from "./pages/Cart";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Routes>
+        <Route index element={<Homepage />}>
+          <Route path="/" element={<Template />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          <Route path="/tos" element={<TosPage />} />
+          <Route path="/products" element={<AllProductsPage />} />
+          <Route path="/products/:productId" element={<SingleProductPage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ src/pages/TosPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TosPage = () => {
+  return <div>TosPage</div>;
+};
+
+export default TosPage;
